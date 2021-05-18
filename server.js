c68934c81119289c6800328d5e7d99313a4e3870
project364 <83402475+project364@users.noreply.github.com>
2021-05-18 13:58:46 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+{
+  "name": "calculator",
+  "version": "1.0.0",
+  "description": "My first Node and Express Server BMI Calculator",
+  "main": "server.js",
+  "scripts": {
+    "test": "echo\"Error:no test specified\" && exit 1",
+    "start": "node server.js"},
+    "author": "Anusha Rani",
+    "license": "ISC"
+  }
+}
